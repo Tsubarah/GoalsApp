fbfd17d67a69f81895c4ba1a42234091d656f98d
Malin Olsson <miragia.solinde@gmail.com>
2022-10-13 13:46:29 +0200

    Updated modalcomponent

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -3,9 +3,24 @@ import { useState } from 'react'
 
 const Modal = () => {
     const [show, setShow] = useState(false)
-    
+
+    const handleClose = () => setShow(false)
+    const handleShow = () => setShow(true)
+
   return (
-    <div>Modal</div>
+        <>
+        <button className="addGoal-button" onClick={handleShow}>Create a Goal</button>
+
+            <div show={show} className="addGoal-modal">
+
+                <button onClick={handleClose}>Close</button>
+
+                <h2>Create a Goal</h2>
+
+                {/* There will be a form here to add goals */}
+
+            </div>
+    </>
   )
 }
 
