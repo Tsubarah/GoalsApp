1ab2e719f994a64e9a006b65a5a8610dada93598
Malin Olsson <miragia.solinde@gmail.com>
2022-10-13 12:05:47 +0200

    Created modal component

---
+++ src/Components/Modal.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useState } from 'react'
+
+const Modal = () => {
+    const [show, setShow] = useState(false)
+    
+  return (
+    <div>Modal</div>
+  )
+}
+
+export default Modal
