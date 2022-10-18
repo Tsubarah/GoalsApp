ca71564bdc0f481ad30de65c84a9b902fbd3dcd6
nikodimi <nikola.dimitrijoski@medieinstitutet.se>
2022-10-18 11:55:51 +0200

    added consultant page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import './App.css';
 import Navbar from './Components/Navbar';
 import ManagerPage from './Pages/ManagerPage';
+import ConsultantPage from './Pages/ConsultantPage';
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
 
             <Routes>
                 <Route path="/" element={<ManagerPage />} />
+                <Route path="/consultant" element={<ConsultantPage />} />
 
             </Routes>
 
