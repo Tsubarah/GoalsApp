0ec0031e8b945092fb98893fa19e44cb30c81556
Malin Olsson <miragia.solinde@gmail.com>
2022-10-18 11:11:41 +0200

    small updates

--- src/Pages/ManagerPage.tsx
+++ src/Pages/ManagerPage.tsx
@@ -1,18 +1,14 @@
 import Sidebar from "../Components/Sidebar"
 import UserList from "../Components/Userlist"
 import './managerPage.css'
-import Modal from "../Components/Modal"
 
 const ManagerPage = () => {
     return (
         <div className="manager-page-wrapper">
-            <Sidebar />
-
-                
-
-            <UserList />
-
-            <Modal />
+                <Sidebar />
+            
+                <UserList />
+           
         </div>
     )
 }
