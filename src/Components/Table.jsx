42e601127946b9c358f70411b53be09cc7c1f946
Benjamin <67978743+Tsubarah@users.noreply.github.com>
2022-10-21 15:17:51 +0200

    fixed table and merged with dev

--- src/Components/Table.jsx
+++ src/Components/Table.jsx
@@ -1,8 +1,8 @@
 import './Table.css'
 import Accordion from './Accordion'
 
-const table = ({ goals }) => {
-  console.log(goals)
+const table = ({ goal }) => {
+  console.log(goal)
   return (
     <div className="table-wrapper">
       <table>
@@ -15,9 +15,8 @@ const table = ({ goals }) => {
             <th className="th-expected">Expected half year progress</th>
             <th className="th-cost">Cost</th>
           </tr>
-          {goals.map((goal, i) => (
             <>
-              <tr key={i}>
+              <tr>
                 <td>{goal.prio}</td>
                 <td>{goal.description}</td>
                 <td>{goal.target}</td>
@@ -33,12 +32,7 @@ const table = ({ goals }) => {
                   <Accordion data={goal.reviews[1]} />
                 </td>
               </tr>
-              {/* <tr key={goal.id}>
-                <th className="half-review">Half Year Review</th>
-                <th className="end-review">End of Year Review</th>
-              </tr> */}
             </>
-          ))}
         </tbody>
       </table>
     </div>
