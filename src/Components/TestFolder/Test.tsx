509b48e3c67bb59832f572c8071935e13922ed05
Tsubarah <Tsubarah@hotmail.se>
2022-10-22 14:35:46 +0200

    added a test folder for components

---
+++ src/Components/TestFolder/Test.tsx
@@ -0,0 +1,16 @@
+// import { TestProps } from './test.types'
+// import styles from './Test'
+
+// const { getTableProps } = useTable({ data })
+
+// const Test = ({ data, id }: TestProps) => {
+//   return (
+//     <table className={styles.Root} {...getTableProps()}>
+//       <thead className={styles.thead}>
+        
+//       </thead>
+//     </table>
+//   )
+// }
+
+// export default Test
