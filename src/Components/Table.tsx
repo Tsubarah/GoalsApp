b00fe96b2bf25742c3338e6fb0459a43853eefb5
Benjamin <67978743+Tsubarah@users.noreply.github.com>
2022-11-01 12:36:07 +0100

    indented table

--- src/Components/Table.tsx
+++ src/Components/Table.tsx
@@ -4,53 +4,53 @@ import EditGoalModal from './EditGoalModal';
 import { IGoal } from '../typings/Goal'
 
 type TabsProps = {
-    goals: IGoal[],
+  goals: IGoal[],
 }
 
-const table = ({ goals }: TabsProps) => {
+const Table = ({ goals }: TabsProps) => {
 
-    return (
-        <div className="table-wrapper">
-            <table>
-                <thead>
-                    <tr>
-                        <th className="th-prio">Prio</th>
-                        <th className="th-deadline">Deadline</th>
-                        <th className="th-description">Goal description</th>
-                        <th className="th-target">Target reached when</th>
-                        <th className="th-milestone">Milestone</th>
-                        <th className="th-expected">
-                            Expected half year progress
-                        </th>
-                        <th className="th-cost">Cost</th>
-                    </tr>
-                </thead>
+  return (
+    <div className="table-wrapper">
+      <table>
+        <thead>
+          <tr>
+            <th className="th-prio">Prio</th>
+            <th className="th-deadline">Deadline</th>
+            <th className="th-description">Goal description</th>
+            <th className="th-target">Target reached when</th>
+            <th className="th-milestone">Milestone</th>
+            <th className="th-expected">
+              Expected half year progress
+            </th>
+            <th className="th-cost">Cost</th>
+          </tr>
+        </thead>
 
-                {goals.map((goal, i) => (
-                    <tbody key={i}>
-                        <tr>
-                            <td>{goal.prio}</td>
-                            <td><Moment format="YYYY/MM/DD">{goal.deadline}</Moment></td>
-                            <td>{goal.description}</td>
-                            <td>{goal.target_reached}</td>
-                            <td>{goal.milestones}</td>
-                            <td>{goal.half_year_progress}</td>
-                            <td>{goal.cost}</td>
-                        </tr>
+        {goals.map((goal, i) => (
+          <tbody key={i}>
+            <tr>
+              <td>{goal.prio}</td>
+              <td><Moment format="YYYY/MM/DD">{goal.deadline}</Moment></td>
+              <td>{goal.description}</td>
+              <td>{goal.target_reached}</td>
+              <td>{goal.milestones}</td>
+              <td>{goal.half_year_progress}</td>
+              <td>{goal.cost}</td>
+            </tr>
 
-                        <tr>
-                            {goal.reviews.map((review,i) => (
-                                <td colSpan={3} key={i}>
-                                    <Accordion data={review} />
-                                </td>
-                            ))}
-                            <EditGoalModal goal={goal} />
-                        </tr>
-                    </tbody>
-                ))}
-            </table>
-        </div>
-    );
+            <tr>
+              {goal.reviews.map((review, i) => (
+                <td colSpan={3} key={i}>
+                  <Accordion data={review} />
+                </td>
+              ))}
+              <EditGoalModal goal={goal} />
+            </tr>
+          </tbody>
+        ))}
+      </table>
+    </div>
+  );
 };
 
-export default table;
+export default Table;
