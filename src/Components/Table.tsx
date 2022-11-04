2827a28f994c14055a196bb7c92db08498285ac8
nddesigns <56118523+nikodimi@users.noreply.github.com>
2022-11-04 20:15:17 +0100

    Refactored table

--- src/Components/Table.tsx
+++ src/Components/Table.tsx
@@ -1,56 +1,84 @@
-import Accordion from "./Accordion";
-import Moment from 'react-moment';
-import EditGoalModal from './EditGoalModal';
+import Accordion from "./Accordion"
+import Moment from 'react-moment'
+import EditGoalModal from "./EditGoalModal"
 import { IGoal } from '../typings/Goal'
 
 type TabsProps = {
-  goals: IGoal[],
+    goals: IGoal[],
 }
 
 const Table = ({ goals }: TabsProps) => {
-  
-  return (
-    <div className="table-wrapper">
-      <table>
-        <thead>
-          <tr>
-            <th className="th-prio">Prio</th>
-            <th className="th-deadline">Deadline</th>
-            <th className="th-description">Goal description</th>
-            <th className="th-target">Target reached when</th>
-            <th className="th-milestone">Milestone</th>
-            <th className="th-expected">
-              Expected half year progress
-            </th>
-            <th className="th-cost">Cost</th>
-          </tr>
-        </thead>
 
-        {goals.map((goal, i) => (
-          <tbody key={i}>
-            <tr>
-              <td>{goal.prio}</td>
-              <td><Moment format="YYYY/MM/DD">{goal.deadline}</Moment></td>
-              <td>{goal.description}</td>
-              <td>{goal.target_reached}</td>
-              <td>{goal.milestones}</td>
-              <td>{goal.half_year_progress}</td>
-              <td>{goal.cost}</td>
-            </tr>
+    return (
+        <>
+            <div className="table-wrapper">
+                <div className="header-wrapper">
+                    <div className="headers header-prio">
+                        <h4>Prio</h4>
+                    </div>
+                    <div className="headers header-deadline">
+                        <h4>Deadline</h4>
+                    </div>
+                    <div className="headers header-description">
+                        <h4>Goal Description</h4>
+                    </div>
+                    <div className="headers header-target">
+                        <h4>Target reached when</h4>
+                    </div>
+                    <div className="headers header-milestone">
+                        <h4>Milestone</h4>
+                    </div>
+                    <div className="headers header-progress">
+                        <h4>Half year progress</h4>
+                    </div>
+                    <div className="headers header-cost">
+                        <h4>Cost</h4>
+                    </div>
+                </div>
+                <div className="body-wrapper">
+                {goals.map((goal, i) => (
+                    <div className="cell-row" key={i}>
 
-            <tr>
-              {goal.reviews.map((review, i) => (
-                <td colSpan={3} key={i}>
-                  <Accordion data={review} />
-                </td>
-              ))}
-              <EditGoalModal goal={goal} />
-            </tr>
-          </tbody>
-        ))}
-      </table>
-    </div>
-  );
-};
+                        <div className="goal-info">
+                            <div className="cells cells-prio">
+                                <h4>{goal.prio}</h4>
+                            </div>
+                            <div className="cells cells-deadline">
+                                <h4><Moment format="YYYY/MM/DD">{goal.deadline}</Moment></h4>
+                            </div>
+                            <div className="cells cells-description">
+                                <h4>{goal.description}</h4>
+                            </div>
+                            <div className="cells cells-target">
+                                <h4>{goal.target_reached}</h4>
+                            </div>
+                            <div className="cells cells-milestone">
+                                <h4>{goal.milestones}</h4>
+                            </div>
+                            <div className="cells cells-progress">
+                                <h4>{goal.half_year_progress}</h4>
+                            </div>
+                            <div className="cells cells-cost">
+                                <h4>{goal.cost}</h4>
+                            </div>
+                        </div>
+                        <div className="reviews">
+                            <div className="revs">
+                                {goal.reviews.map((review, i) => (
+                                    <div className="accordion" key={i}>
+                                        <Accordion data={review} />
+                                    </div>
+                                ))}
+                                <EditGoalModal goal={goal} />
+                            </div>
+                        </div>
+                    </div>
+                    
+                    ))}
+                </div>
+            </div>
+        </>
+    )
+}
 
-export default Table;
+export default Table
