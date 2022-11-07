5b900658e2d1ad569c009d336675a7c5c99f491d
nikodimi <nikola.dimitrijoski@medieinstitutet.se>
2022-11-07 15:41:33 +0100

    Fetching and displaying user info

--- src/Components/UserInfo.tsx
+++ src/Components/UserInfo.tsx
@@ -1,16 +1,50 @@
 import Profile from "./Profile";
+import { FC, useEffect, useState } from "react";
+import { useAuth } from "../services/auth";
+import useUsers from "../services/useUsers";
+// import { User } from '../typings/User'
 
-const UserInfo = () => {
+type User = {
+  displayName?: string,
+  id?: string,
+  jobTitle?: string,
+  mail?: string,
+  mobilePhone?: number
+}
+
+const UserInfo:FC = () => {
+  const { accessToken } = useAuth();
+  const { getUserDetails } = useUsers()
+  const [userData, setUserData] = useState<User>();
+
+  useEffect(() => {
+    if (!accessToken) {
+      return;
+    }
+    
+    async function getUser(accessToken: string) {
+      const user = await getUserDetails(accessToken)
+      if (user) {
+        console.log(user)
+        setUserData(user)
+        console.log(userData)
+      }
+    }
+    getUser(accessToken)
+  },[accessToken])
+
     return (
       <div className="user-wrapper">
         <div className="user">
+          {userData && (
           <div className="user-stats">
-              <h4>Mail: john.doe@something.se</h4>
-              <h4>ID: ee34ep-282a-4b5f-b41a-967c6vc9-43oslv</h4>
-              <h4>Goals: 4</h4>
+            <h4>Mail: {userData.mail}</h4>
+            <h4>ID: {userData.id}</h4>
+            <h4>Phone number: {userData.mobilePhone}</h4>
           </div>
+            )}
           <div className="user-profile">
-            <Profile />
+            <Profile userdata={userData}/>
           </div>
         </div>
 
