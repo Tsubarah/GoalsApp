42e1360b4a18e07236f6348559064d95b66dfa91
Malin Olsson <miragia.solinde@gmail.com>
2022-11-07 08:16:03 +0100

    testing div tables

---
+++ src/Components/OtherTable.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const OtherTable = () => {
+  return (
+    <div className='tableNumber2'>
+        <div className='tableHeader2'>
+            <div className='prioTable'>Prio</div>
+            <div className='deadlineTable'>Deadline</div>
+            <div className='goalTable'>Goal description</div>
+            <div className='targetTable'>Target reached When</div>
+            <div className='milestoneTable'>Milestone</div>
+            <div className='halfYearTable'>Half Year progress</div>
+            <div className='costTable'>Cost</div>
+        </div>
+        <div></div>
+    </div>
+  )
+}
+
+export default OtherTable
