9b519fd599e89c108b4e9cf51b64bfc7d8b67ba9
Malin Olsson <miragia.solinde@gmail.com>
2022-11-11 09:24:04 +0100

    commenting out code

--- src/Contexts/AuthContext.tsx
+++ src/Contexts/AuthContext.tsx
@@ -1,39 +1,39 @@
-import { createContext, useContext, useEffect, useState } from 'react'
-import { useAuth } from '../services/auth'
-import { useMsal } from '@azure/msal-react'
+// import { createContext, useContext, useEffect, useState } from 'react'
+// import { useAuth } from '../services/auth'
+// import { useMsal } from '@azure/msal-react'
 
   
-const AuthContext = createContext(null)
+// const AuthContext = createContext(null)
 
-const useAuthContext = () => useContext(AuthContext)
+// const useAuthContext = () => useContext(AuthContext)
 
-const AuthProvider = ({ children }) => {
-  const { instance, accounts } = useMsal()
-  const [currentUser, setCurrentUser] = useState(null)
-  const [userEmail, setUserEmail] = useState(null)
-  const [accessToken, setAccessToken] = useState<string | undefined>()
+// const AuthProvider = ({ children }) => {
+//   const { instance, accounts } = useMsal()
+//   const [currentUser, setCurrentUser] = useState(null)
+//   const [userEmail, setUserEmail] = useState(null)
+//   const [accessToken, setAccessToken] = useState<string | undefined>()
   
-  const auth = useAuth()
+//   const auth = useAuth()
 
   
-  useEffect(()=> {
+//   useEffect(()=> {
 
-  },[])
+//   },[])
 
-  const contextValues = {
-    currentUser,
-    userEmail,
-    accessToken,
-    instance,
-    accounts,
+//   const contextValues = {
+//     currentUser,
+//     userEmail,
+//     accessToken,
+//     instance,
+//     accounts,
 
-  }
+//   }
 
-  return <AuthContext.Provider value={auth}>
-    {children}
-  </AuthContext.Provider>
+//   return <AuthContext.Provider value={auth}>
+//     {children}
+//   </AuthContext.Provider>
   
-}
+// }
 
 
-export { useAuthContext, AuthProvider}
+// export { useAuthContext, AuthProvider}
