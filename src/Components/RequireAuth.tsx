e8a01ac1299d9149d5dbc22f210348c64eb035e5
Malin Olsson <miragia.solinde@gmail.com>
2022-11-11 09:33:44 +0100

    wip

--- src/Components/RequireAuth.tsx
+++ src/Components/RequireAuth.tsx
@@ -1,18 +1,18 @@
-// import { Navigate } from 'react-router-dom'
-// import { useAuth } from '../services/auth'
+import { Navigate } from 'react-router-dom'
+import { useAuth } from '../services/auth'
 
 
 
-// const RequireAuth = (
-//     children,
-//     redirectTo = "/Auth",
-// ) => {
-//     const { user } = useAuth()
-//   return (
-//     user
-//     ? children
-//     : <Navigate to={redirectTo}
-//   )
-// }
+const RequireAuth = (
+    children,
+    redirectTo = "/auth",
+) => {
+    const { user } = useAuth()
+  return (
+    user
+    ? children
+    : <Navigate to={redirectTo}
+  )
+}
 
-// export default RequireAuth
+export default RequireAuth
