e01c3d94f894b4da69dff7d00808f4438be62c91
nikodimi <nikola.dimitrijoski@medieinstitutet.se>
2022-11-15 14:06:24 +0100

    Added protected routes with an require auth component

---
+++ src/Components/RequireAuth.tsx
@@ -0,0 +1,21 @@
+
+import { Navigate } from 'react-router-dom'
+import { useAuthContext } from '../Contexts/AuthContext'
+
+export type Props = {
+    children: JSX.Element,
+}
+
+const RequireAuth = ({
+	children,
+}:Props) => {
+	const { currentUser } = useAuthContext()
+
+	return (
+		currentUser
+			? children
+			: <Navigate to= "/"/>
+	)
+}
+
+export default RequireAuth
