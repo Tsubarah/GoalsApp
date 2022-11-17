cf6390f2969170d2aabdb3ffcdef7da114aa3381
Malin Olsson <miragia.solinde@gmail.com>
2022-11-17 14:30:46 +0100

    styling changes, and started to do restrictions on buttons

--- src/Components/EditGoalModal.tsx
+++ src/Components/EditGoalModal.tsx
@@ -1,25 +1,28 @@
 import { useState } from "react"
 import EditGoalsForm from "./EditGoalsForm"
 import { IGoal } from '../typings/Goal'
-
+// import { IUser } from '../typings/User'
 type ModalProps = {
   goal: IGoal,
+  // user: IUser,
   setSlide: (slide: string) => void,
 }
 
 const EditGoalModal = ({ goal, setSlide }: ModalProps) => {
   const [show, setShow] = useState(false)
   const [isComplete, setIsComplete] = useState(goal.isComplete)
+  // const [isManager, setIsManager] = useState(user.jobTitle.includes('Manager'))
 
   return (
     <>
+    {/* { isManager ? */}
       <button 
         className="button edit-btn" 
         onClick={() => {setShow(!show)}}
       >
         {isComplete ? "View": "Edit"}
       </button>
-
+    {/* : ""} */}
       {
         show && (
 
@@ -40,7 +43,7 @@ const EditGoalModal = ({ goal, setSlide }: ModalProps) => {
           
           <hr />
           
-          <EditGoalsForm goal={goal} setShow={setShow} show={show} setSlide={setSlide} />
+           <EditGoalsForm setShow={setShow} show={show} setSlide={setSlide} goal={goal} /> 
             
         </div>
         )
--- src/Components/HistoryList.tsx
+++ src/Components/HistoryList.tsx
@@ -4,8 +4,8 @@ import EditGoalModal from "./EditGoalModal";
 import Moment from "react-moment";
 
  type HistoryProps = {
-     goals: IGoal[],
-
+    goals: IGoal[],
+    
  }
 
 
@@ -59,10 +59,10 @@ const HistoryList = ({ goals }:HistoryProps) => {
 
              <div className="history-list-wrapper">
                 <div className="history-header">
-                    <div className="history-deadline"><h3>Creation Date </h3></div>
-                    <div className="history-category"><h3>Category</h3></div>
-                    <div className="history-description"><h3>Goal Description</h3></div>
-                    <div className="history-creationDate"><h3>Deadline</h3></div>
+                    <div className="history-creationDate"><h3>Creation Date </h3></div>
+                    <div className="history-category category"><h3>Category</h3></div>
+                    <div className="history-description description"><h3>Goal Description</h3></div>
+                    <div className="history-deadline deadline"><h3 className="deadline">Deadline</h3></div>
                 </div>
                 <ul>
                 {completedGoals.map((goal, i)=> (
@@ -71,10 +71,10 @@ const HistoryList = ({ goals }:HistoryProps) => {
                         <div className="history-list-item">
                         
                            <div className="history-list-div">
-                                <div className="history-deadline"><Moment format="YYYY/MM/DD">{goal.creationDate}</Moment></div>
+                                <div className="history-creationDate"><Moment format="YYYY/MM/DD">{goal.creationDate}</Moment></div>
                                 <div className="history-category"> {goal.category.replace(/([a-z])([A-Z])/g, '$1 $2').toUpperCase()}</div>
                                 <div className="history-description">{goal.description}</div>
-                                <div className="history-creationDate"><Moment format="YYYY/MM/DD">{goal.deadline}</Moment></div>
+                                <div className="history-deadline deadline"><Moment format="YYYY/MM/DD">{goal.deadline}</Moment></div>
                             </div>
                             <div className="history-button-holder">
                                 <EditGoalModal setSlide={setSlide} goal={goal}/>
