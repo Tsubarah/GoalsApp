aeb3e6d014f460ca65b0b62c71837f9d5f4035d6
nikodimi <nikola.dimitrijoski@medieinstitutet.se>
2022-12-01 10:37:40 +0100

    api

--- src/services/GoalsAPI.ts
+++ src/services/GoalsAPI.ts
@@ -4,7 +4,7 @@ import { IGoal } from '../typings/Goalinterface'
  */
 import axios from 'axios'
  
-axios.defaults.baseURL = 'http://localhost:7071/api'
+axios.defaults.baseURL = 'https://api.goalsnow.geshdo.dev/'
 
 /**
   * GET an endpoint
