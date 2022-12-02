b99d831289e1accde7d2b1ea7224090afb55d6e6
Tsubarah <Tsubarah@hotmail.se>
2022-12-02 13:11:49 +0100

    .

--- src/authConfig.ts
+++ src/authConfig.ts
@@ -1,30 +1,29 @@
-import {  PublicClientApplication } from "@azure/msal-browser";
+// import {  PublicClientApplication } from "@azure/msal-browser";
 
-let auth = {
-  clientId: "3d444f78-10b0-4dca-9667-f8847df74a0b",
-  validIssuer:
-    "https://login.microsoftonline.com/cd20e4c9-f82c-4d3e-9224-90f2bc4be1a0/v2.0",
-  authority: "https://login.microsoftonline.com/geshdo.com",
-  redirectUri: "https://goalsnow.geshdo.dev", // https://goalsnow.geshdo.dev in dev
-  postLogoutRedirectUri: "https://goalsnow.geshdo.dev",
-};
+// let auth = {
+//   clientId: "3d444f78-10b0-4dca-9667-f8847df74a0b",
+//   validIssuer:
+//     "https://login.microsoftonline.com/cd20e4c9-f82c-4d3e-9224-90f2bc4be1a0/v2.0",
+//   authority: "https://login.microsoftonline.com/geshdo.com",
+//   redirectUri: "https://goalsnow.geshdo.dev", // https://goalsnow.geshdo.dev in dev
+//   postLogoutRedirectUri: "https://goalsnow.geshdo.dev",
+// };
 
-export const msalConfig = {
-  auth: {
-    clientId: auth.clientId,
-    authority: auth.authority,
-    validIssuer: auth.validIssuer,
-    redirectUri: auth.redirectUri,
-    postLogoutRedirectUri: auth.postLogoutRedirectUri,
-  },
-};
+// export const msalConfig = {
+//   auth: {
+//     clientId: auth.clientId,
+//     authority: auth.authority,
+//     validIssuer: auth.validIssuer,
+//     redirectUri: auth.redirectUri,
+//     postLogoutRedirectUri: auth.postLogoutRedirectUri,
+//   },
+// };
 
-export const loginRequest = {
-  scopes: ["User.Read"],
-};
+// export const loginRequest = {
+//   scopes: ["User.Read"],
+// };
 
-const MSALobj = new PublicClientApplication(msalConfig);
+// const MSALobj = new PublicClientApplication(msalConfig);
 
-export const logoutReq = {
-  account: MSALobj.getAccountByHomeId(auth.clientId),
-}
+// export const logoutReq = {
+//   account: MSALobj.getAccountByHomeId(auth.clientId),
