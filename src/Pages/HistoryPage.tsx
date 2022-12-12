9970f6dbf0281f32766c01662d8461dc6dfe821d
nikodimi <nikola.dimitrijoski@medieinstitutet.se>
2022-12-12 10:16:25 +0100

    Current user can now click on their own goals from start

--- src/Pages/HistoryPage.tsx
+++ src/Pages/HistoryPage.tsx
@@ -1,41 +1,37 @@
-import { useEffect, useState } from "react"
-import { useQuery } from "react-query"
-import { useParams } from "react-router-dom"
-import GoalsAPI from "../services/GoalsAPI"
-import HistoryList from "../Components/HistoryList"
-import UserInfo from "../Components/UserInfo"
-import { IGoal } from "../typings/Goalinterface"
-import { IUser } from "../typings/Userinterface"
+import { useEffect, useState } from 'react'
+import { useQuery } from 'react-query'
+import { useParams } from 'react-router-dom'
+import GoalsAPI from '../services/GoalsAPI'
+import HistoryList from '../Components/HistoryList'
+import UserInfo from '../Components/UserInfo'
+import { IGoal } from '../typings/Goalinterface'
+import { IUser } from '../typings/Userinterface'
 import useLocalStorage from "../Hooks/useLocalStorage"
 import { useAuthContext } from "../Contexts/AuthContext"
 
 const HistoryPage = () => {
   const [user, setUser] = useState<IUser | undefined>()
-  const { id } = useParams()
   const { currentUser } = useAuthContext()
-  const [target, setTarget] = useLocalStorage("target")
   const [inCompletedGoals, setIncompletedGoals] = useState<IGoal[]>()
+  const { id } = useParams()
+  const [target, setTarget] = useLocalStorage("target")
   const [goals, setGoals] = useLocalStorage(
-    id == target.id
-      ? target.id.toString()
-      : id == currentUser?.id
+    id === currentUser?.id.toString()
       ? currentUser?.id
+      : id === target?.id.toString()
+      ? target?.id
       : "",
     []
   )
+                                                
+  console.log('currentUser', currentUser)
 
   useEffect(() => {
     if (!user) {
       setUser(target)
     }
-    setIncompletedGoals(goals?.filter((goal: IGoal) => !goal.isComplete))
+    setIncompletedGoals(goals?.filter((goal: IGoal) => goal.isComplete))
   }, [goals])
-
-  useEffect(() => {
-    let targets: any = window.localStorage.getItem("target")
-    let target = JSON.parse(targets)
-    setUser(target)
-  }, [])
 
   useEffect(() => {
     if (!goals) return
